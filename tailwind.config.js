249166ebcb211761579db2d9f59e12e16d73fd93
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2024-11-19 23:13:13 +0700

    Update new ui page, contact & about

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["login.html", "register.html", "menu.html", "cart.html", "index.html", "register.html", "productDetail.html"],
+  content: ["login.html", "register.html", "menu.html", "cart.html", "index.html", "register.html", "productDetail.html", "contact.html", "about.html"],
   theme: {
     extend: {
       fontFamily: {
